8b1692cf4a325cbfb05d8433e744441e8266ec83
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2022-05-31 01:09:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Иванов";
+            string secondName = "Иван";
+            string changer;
+
+            Console.WriteLine("Неверные Имя и Фамилия: {0} {1}", name, secondName);
+
+            changer = name;
+            name = secondName;
+            secondName = changer;
+
+            Console.WriteLine("Bерные Имя и Фамилия: {0} {1}", name, secondName);
+        }
+    }
+}
